a4a7ac3ea22ce0c996df4169dafcbafa27548bda
hellomyzn <em711th@icloud.com>
2023-09-18 22:46:28 +0900

    update: config

--- docker-python/src/common/config/config.py
+++ docker-python/src/common/config/config.py
@@ -1,3 +1,4 @@
+"""common.config.Config"""
 #########################################################
 # Builtin packages
 #########################################################
@@ -14,10 +15,19 @@ from utils import Singleton
 
 
 class Config(Singleton):
+    __config = None
+
     def __init__(self):
-        self.config_file = 'common/config/config.ini'
+        self.__config_file = 'common/config/config.ini'
+
+        if self.__config is None:
+            self.__config = ConfigParser()
+            self.__config.read(self.__config_file, encoding="utf-8")
 
     def get_config(self) -> ConfigParser:
-        config = ConfigParser()
-        config.read(self.config_file, encoding="utf-8")
-        return config
+        """Getter for __config
+
+        Returns:
+            self.__config (ConfigParser): Private property __config
+        """
+        return self.__config
