c55c68bc4553ecd49e29ac2a7c73c5034f5adb4f
hellomyzn <em711th@icloud.com>
2023-09-19 09:33:02 +0900

    update: singleton in spotify

--- docker-python/src/common/spotify/spotify_accessor.py
+++ docker-python/src/common/spotify/spotify_accessor.py
@@ -14,6 +14,7 @@ from spotipy.oauth2 import SpotifyOAuth
 # Own packages
 #########################################################
 from utils import Singleton
+
 from common.config import Config
 from common.logging import (
     info,
@@ -33,15 +34,14 @@ class SpotifyAccessor(Singleton):
     __connection = None
 
     def __init__(self):
-        self.__connection = SpotifyAccessor.initialize()
+        self.__connection = self.__initialize()
 
     @property
-    def get_connection(self):
+    def connection(self):
         """Getter for __connection"""
         return self.__connection
 
-    @classmethod
-    def initialize(cls) -> spotipy.client.Spotify:
+    def __initialize(self) -> spotipy.client.Spotify:
         """ Connect spotify api by SpotifyOAuth.
 
         Scope reference:    https://developer.spotify.com/documentation/general/guides/authorization/scopes/
@@ -54,10 +54,10 @@ class SpotifyAccessor(Singleton):
         Returns:
             __connection (spotipy.client.Spotify): spotify connection
         """
-        if cls.__connection is not None:
-            return cls.__connection
+        if self.__connection is not None:
+            return self.__connection
 
-        config = Config().get_config()
+        config = Config().config
         client_id = config['SPOTIPY']['SPOTIPY_CLIENT_ID']
         client_secret = config['SPOTIPY']['SPOTIPY_CLIENT_SECRET']
         redirect_uri = 'https://google.com'
@@ -74,9 +74,10 @@ class SpotifyAccessor(Singleton):
                                     scope=scope,
                                     open_browser=False)
 
+        connection = None
         try:
             # Connect spotify
-            cls.__connection = spotipy.Spotify(auth_manager=auth_manager, language='en')
+            connection = spotipy.Spotify(auth_manager=auth_manager, language='en')
             info('Succeed in connecting Spotify...')
         except Exception as err:
             error_stack_trace(
@@ -86,4 +87,4 @@ class SpotifyAccessor(Singleton):
                 redirect_uri: {redirect_uri}, \
                 scope: {scope}")
 
-        return cls.__connection
+        return connection
